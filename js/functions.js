1167a1daadbf891d50998dce8461e58d99f7bb93
Luillin Escobar <contacto@contratistaweb.com>
2019-11-12 23:06:27 -0500

    inicio desarrollo taller js

---
+++ js/functions.js
@@ -0,0 +1,35 @@
+// ¿Cuantos peces hay en el estanque
+var pecesRojos = 461;
+var pecesAzules = 217;
+document.write("<h3>¿Cuantos peces hay en el acuario?</h3><p>Peces rojos: "+pecesRojos+"<br>Peces azules: "+pecesAzules+"</p>");
+
+// ¿cuanto se ha gastado Carmen?
+var dineroInicial = 50000;
+var saldo = 15000;
+var gasto = dineroInicial - saldo;
+document.write("<h3>¿Cuanto se ha gastado Carmen?</h3><p>carmen se ha gastado: $"+gasto+"</p>");
+
+
+// ¿Cuánto dinero me he gastado en total?
+var camisetas = 17478*3;
+var zapatillas = 84633;
+var pantalones = 110391;
+var descuento = 29437;
+var pago = ( camisetas + zapatillas + pantalones ) - descuento
+document.write("<h3>¿Cuánto dinero me he gastado en total?</h3><p>En total en la compra online me gaste: $"+pago+"</p>");
+
+
+//¿Cuánto te ha costado el ordenador?
+var costo = 2428623;
+var dctoProntoPago = 0.1;
+var pagoTotal = costo - ( costo * dctoProntoPago );
+document.write("<h3>¿Cuánto te ha costado el ordenador?</h3><p>El ordenador me costo: XXX hallar $"+parseInt( pagoTotal )+"</p>");
+
+
+// ¿Qué precio tenían antes del descuento?
+var vrPagado = 125110;
+var dctoCompra = 0.2
+var vrTotalCompra = ''
+
+document.write("<h3>¿Qué precio tenían antes del descuento?</h3><p>el precio antes de descuento de: XXX hallar $"++"</p>");
+
